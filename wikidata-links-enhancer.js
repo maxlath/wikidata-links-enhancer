910cd8bb0f018238fb758d287c2e874781249f88
maxlath <k@maxlath.eu>
2014-11-13 19:53:30 +0100

    first commit

---
+++ wikidata-links-enhancer.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name Wikidata Link
+// @namespace wikimedia
+// @description displays wikidata links under any wikimedia projects pages
+// @include *.wikipedia.org/*
+// @include *.wikisource.org/*
+// @include *.wikiquote.org/*
+// @include *.wikimedia.org/*
+// @version 1
+// @grant none
+// ==/UserScript==
+
+
+var getLink = function () {
+  return $('a').filter(function (index) {
+    return /wikidata.org\/wiki\/Q/.test($(this).attr('href'))
+  }).first().attr('href');
+};
+
+var extratWikidataId = function (href) {
+  return href.split('wikidata.org/wiki/') [1]
+};
+
+var addLinkAfterHeader = function (href) {
+  if (href) {
+    var id = extratWikidataId(href);
+    console.log('wikidata link found', href, id);
+    $el = $(document.createElement('a')).attr('href', href).text(id);
+    $('h1').next().prepend($el);
+  }
+};
+
+// after DOM ready, get link and add it under the page h1
+$(function () {
+  addLinkAfterHeader(getLink())
+});
